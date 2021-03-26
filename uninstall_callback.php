c35d564bb296f9b56a3ecbe1d315469b150a6d9e
vilas247 <vilas@247commerce.co.uk>
2021-03-26 17:28:31 +0530

    deleting all files

--- uninstall_callback.php
+++
@@ -1,8 +0,0 @@
-<?php
-   ////$data = json_decode(file_get_contents('php://input'), true);
-   $data = $_REQUEST;
-   $fp = fopen("uninstall.txt", "w");
-   fwrite($fp, serialize($data));
-   fclose($fp);
-
-?>
